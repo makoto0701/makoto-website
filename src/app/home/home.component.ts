3c98a6e522664bd670e6647f26d63e0df290a0db
makoto <makoto.study0701@gmail.com>
2019-09-28 21:42:10 +0900

    again

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  title = 'Welcome to Ignite UI for Angular!';
+  title = 'Makoto Website';
   constructor() { }
 
   ngOnInit() {
